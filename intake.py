216129dc014a376d66166ec28adb1fbff815f835
RenRainville <mark.rainville@students.iaac.net>
2023-08-26 09:54:30 -0700

    coninuted trials to get a response

--- intake.py
+++ intake.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
 import compute_rhino3d.Util
 import compute_rhino3d.Grasshopper as gh
-
+import rhino3dm
 
 compute_rhino3d.Util.url = "http://localhost:8081/"
 #compute_rhino3d.Util.apiKey = ""
 
 defName = 'assets/FloorPlanGenerator_Combined.gh'
 
-file_path = 'assets/DataFromJS.json'
+file_path = 'assets/dataObject.json'
 
 try:
     with open(file_path, 'r') as file:
@@ -36,6 +36,7 @@ try:
     corrType = data['properties']['corridorType']
     Dict = {"double loaded": 0, "single loaded": 1}
     corridor = Dict[corrType]
+    # print(corridor)
     corr_tree = gh.DataTree('RH_IN:corridor')
     corr_tree.Append([0], [corridor])
     # corridor = corr_tree
@@ -48,17 +49,29 @@ except Exception as e:
     print(f'Error constructing DataTree: {e}.')
 
 trees = [lineType_tree, vertices_tree, corr_tree, levels_tree]
+# trees = [lineType, vertices, corrType, levels]
 
-trees_data = [tree.data for tree in trees]
-print(trees_data)
-
-# output = gh.EvaluateDefinition(defName, trees)
-# print(output)
-
+# trees_data = [tree.data for tree in trees]
+# print(trees_data)
+try:
+    output = gh.EvaluateDefinition(defName, trees)
+    string_data = output[0].InnerTree.First().Value[0].Data
+    print(string_data)
+except Exception as e:
+    print(f'Error running grasshopper file: {e}.')
+# decode results
+# branch = output['json_string'][0]['InnerTree']['{ 0; }']
+# lines = [rhino3dm.CommonObject.Decode(json.loads(item['data'])) for item in branch]
+# string = [rhino3dm.CommonObject.Decode(json.loads(output))]
+if output:
+    # writing JSON data
+    with open("assets/output.json", 'w') as f:
+        json.dump(string_data, f)
 # Convert the dictionary to a JSON string
 # json_string = json.dumps(output, indent=4)
-json_string = json.dumps(output)
+# json_string = json.dumps(string, indent=4)
+# json_string = json.dumps(output)
 
 # Write the JSON string to a file
-with open("assets/output.json", "w") as json_file:
-    json_file.write(json_string)
+# with open("assets/output.json", "w") as json_file:
+#     json_file.write(json_string)
