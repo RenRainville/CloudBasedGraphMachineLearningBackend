b1b6dd8ea0198f981adf9b5f22dd2c08c978131f
RenRainville <mark.rainville@students.iaac.net>
2023-09-01 06:48:21 -0700

    former error is gone, now output is empty

--- intake.py
+++ intake.py
@@ -1,10 +1,10 @@
 import json
-import requests
+# import requests
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
 import compute_rhino3d.Util
 import compute_rhino3d.Grasshopper as gh
-import rhino3dm
+# import rhino3dm
 
 compute_rhino3d.Util.url = "http://localhost:8081/"
 compute_rhino3d.Util.apiKey = ""
@@ -25,13 +25,16 @@ except json.JSONDecodeError:
 
 try:
     lineType = data['geometry']['type']
+    # print(lineType)
     lineType_tree = gh.DataTree('RH_IN:lineType')
     lineType_tree.Append([0], [lineType])
     # lineType = lineType_tree
 
-    vertices = data['geometry']['coordinates'][0]
+    vertices = data['geometry']['coordinates'][0:]
+    # print(vertices)
     vertices_tree = gh.DataTree('RH_IN:vertices')
-    vertices_tree.Append([0], [vertices])
+    for v in range(len(vertices)):
+        vertices_tree.Append([0], [v])
     # vertices = vertices_tree
 
     corrType = data['properties']['corridorType']
@@ -43,6 +46,7 @@ try:
     # corridor = corr_tree
 
     levels = data['properties']['floorsInput']
+    # print(levels)
     levels_tree = gh.DataTree('RH_IN:levels')
     levels_tree.Append([0], [levels])
     # levels = levels_tree
@@ -52,8 +56,8 @@ except Exception as e:
 trees = [lineType_tree, vertices_tree, corr_tree, levels_tree]
 # trees = [lineType, vertices, corrType, levels]
 
-# trees_data = [tree.data for tree in trees]
-# print(trees_data)
+trees_data = [tree.data for tree in trees]
+print(trees_data)
 try:
     output = gh.EvaluateDefinition(defName, trees)
     # string_data = output[0].InnerTree.First().Value[0].Data
@@ -63,7 +67,6 @@ except Exception as e:
     print(f'Error running grasshopper file: {e}.')
 # decode results
 # branch = output['json_string'][0]['InnerTree']['{ 0; }']
-# lines = [rhino3dm.CommonObject.Decode(json.loads(item['data'])) for item in branch]
 # string = [rhino3dm.CommonObject.Decode(json.loads(output))]
 if output:
     # writing JSON data
