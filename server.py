a2d417e4494e86c1e4304c3cd42eb7ff0f15a190
RenRainville <mark.rainville@students.iaac.net>
2023-09-03 16:00:50 -0700

    testing intake

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from waitress import serve
 # from customPythonFile import customPythonFunction
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -12,12 +13,28 @@ def index():
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
-def process_data():
+
+def intakeData():
     # Get JSON data from the request
     data = request.get_json()
+    with open("assets/dataObject.json", 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    return data
 
+def execute(filename):
+    f = open(filename, "r")
+    r = (f.read())
+    f.close()
+    return r
+
+def process_data():
+    # exec(open('intake.py').read())
+    # execute intake.py to process web output to graph form
+    execute('intake.py')
     # Do some processing here...
-    processed_data = data  # Just echo the data back
+    execute('predict.py')
+    
+    processed_data = execute('output.json') # Just echo the data back
 
     # Return processed data as JSON
     return jsonify(processed_data)
