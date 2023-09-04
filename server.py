df3ed3b77bb5ad6ab3785971d80f0a2761d78b72
RenRainville <mark.rainville@students.iaac.net>
2023-09-04 12:48:52 -0700

    revised intake.py to be a defined function

--- server.py
+++ server.py
@@ -3,9 +3,13 @@ from waitress import serve
 # from customPythonFile import customPythonFunction
 from flask_cors import CORS
 import json
+import subprocess
+from intake import process_intake
 
 app = Flask(__name__)
 CORS(app)
+url = "http://localhost:8081/"
+api_key = ""
 
 @app.route('/')
 @app.route('/index')
@@ -13,8 +17,20 @@ def index():
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
+def process_data():
+    result = process_intake(url, api_key)
+    # exec(open('intake.py').read())
+    # execute intake.py to process web output to graph form
+    # execute('intake.py')
+    # Do some processing here...
+    execute('predict.py')
+    
+    processed_data = execute_json('predicted_classes.json') # Just echo the data back
 
-def intakeData():
+    # Return processed data as JSON
+    return jsonify(processed_data)
+
+def intake_data():
     # Get JSON data from the request
     data = request.get_json()
     with open("assets/dataObject.json", 'w') as json_file:
@@ -22,22 +38,13 @@ def intakeData():
     return data
 
 def execute(filename):
-    f = open(filename, "r")
-    r = (f.read())
-    f.close()
-    return r
+    with open(filename, 'r') as file:
+        code = file.read()
+        exec(code)
 
-def process_data():
-    # exec(open('intake.py').read())
-    # execute intake.py to process web output to graph form
-    execute('intake.py')
-    # Do some processing here...
-    execute('predict.py')
-    
-    processed_data = execute('output.json') # Just echo the data back
-
-    # Return processed data as JSON
-    return jsonify(processed_data)
+def execute_json(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
